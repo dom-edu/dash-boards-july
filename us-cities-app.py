d6dc56379e2ac9b5176bd603bafc1d46a167c393
dom-edu <84454639+dom-edu@users.noreply.github.com>
2025-07-26 15:49:40 -0400

    added us-cities app

---
+++ us-cities-app.py
@@ -0,0 +1,36 @@
+import pandas as pd 
+import plotly.express as px
+from dash import Dash, html, dcc 
+
+url = "https://raw.githubusercontent.com/plotly/datasets/refs/heads/master/2014_us_cities.csv"
+
+cities_df = pd.read_csv(url)
+
+# too much data, let's slim it down to top_10
+top_10_cities = cities_df.sort_values('pop', ascending=False).head(10)
+
+
+# add a plotly bar chart 
+x = top_10_cities['name'] # get city names 
+y = top_10_cities['pop'] # get city populations
+bar_ = px.bar(x=x, y=y)
+
+
+# change axis labels, usually 
+# how we edit styles in dash 
+bar_.update_layout(
+    xaxis_title = "City",
+    yaxis_title = "Pop"
+)
+
+
+# instantiate the dash app 
+app = Dash(__name__)
+
+# app layout 
+app.layout = [
+    html.Div(children="US City Populations", style={'textAlign':'center'}),
+    dcc.Graph(figure=bar_)
+]
+
+app.run(debug=True, port=5000)
