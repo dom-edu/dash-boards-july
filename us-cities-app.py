aa4a7fde9781ac544f7051052231073bb8e9039d
dom-edu <84454639+dom-edu@users.noreply.github.com>
2025-07-26 16:15:25 -0400

    added multi select cities dropdown for comparisons

--- us-cities-app.py
+++ us-cities-app.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 import plotly.express as px
-from dash import Dash, html, dcc 
+from dash import Dash, html, dcc ,  Input, Output,callback
 
 url = "https://raw.githubusercontent.com/plotly/datasets/refs/heads/master/2014_us_cities.csv"
 
@@ -21,6 +21,7 @@ bar_ = px.bar(x=x, y=y)
 dd_1 = dcc.Dropdown(
     x,
     x[:2], # by default select first two 
+    id="cities-dd",
     multi=True
 )
 
@@ -40,7 +41,39 @@ app = Dash(__name__)
 app.layout = [
     html.Div(children="US City Populations", style={'textAlign':'center'}),
     dd_1,
-    dcc.Graph(figure=bar_)
+    dcc.Graph(figure=bar_, id="cities-bar-chart")
 ]
+
+# we want to register a call back 
+@callback(
+    Output('cities-bar-chart', 'figure'),
+    Input('cities-dd', 'value')
+)
+def update_graph(value):
+    print("DEBUG:", value)
+
+    # filter the dataframe by selected value 
+    filter_ = cities_df['name'].isin(value) # filter by selected values
+
+
+    cities_sel_df = cities_df[filter_]
+    x_ = cities_sel_df['name']
+    y_ = cities_sel_df['pop']
+
+    # remake the bar_chart 
+    bar_ = px.bar(x= x_, y=y_)
+
+    # bar chart styles
+    bar_.update_layout(
+        xaxis_title = "City",
+        yaxis_title = "Pop"
+    )
+    
+    return bar_
+
+
+
+
+
 
 app.run(debug=True, port=5000)
