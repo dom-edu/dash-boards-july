dcc0994a7944bd8f80192b64a43d4a50f1df64bc
dom-edu <84454639+dom-edu@users.noreply.github.com>
2025-07-26 15:53:44 -0400

    added DropDown Component

--- us-cities-app.py
+++ us-cities-app.py
@@ -6,6 +6,7 @@ url = "https://raw.githubusercontent.com/plotly/datasets/refs/heads/master/2014_
 
 cities_df = pd.read_csv(url)
 
+
 # too much data, let's slim it down to top_10
 top_10_cities = cities_df.sort_values('pop', ascending=False).head(10)
 
@@ -14,6 +15,13 @@ top_10_cities = cities_df.sort_values('pop', ascending=False).head(10)
 x = top_10_cities['name'] # get city names 
 y = top_10_cities['pop'] # get city populations
 bar_ = px.bar(x=x, y=y)
+
+# drop down 
+dd_1 = dcc.Dropdown(
+    ['New York City', 'Montreal', 'San Francisco'],
+    ['Montreal', 'San Francisco'],
+    multi=True
+)
 
 
 # change axis labels, usually 
@@ -30,6 +38,7 @@ app = Dash(__name__)
 # app layout 
 app.layout = [
     html.Div(children="US City Populations", style={'textAlign':'center'}),
+    dd_1,
     dcc.Graph(figure=bar_)
 ]
 
