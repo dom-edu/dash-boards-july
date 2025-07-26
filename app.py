54dd98f1fc0ca3cdf506da3f942da78738ab61aa
dom-edu <84454639+dom-edu@users.noreply.github.com>
2025-07-26 12:27:18 -0400

    add route exercise

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 # (that we installed with pip3 install flask)
 from flask import Flask, render_template
 
+GREETINGS = ['Hello', "Good Day", "Good Night", "Welcome!"]
+
 # instantiating the flask app 
 app = Flask(__name__)
 
@@ -27,6 +29,12 @@ def about():
     return about page to user
     """
     return "<p>About Me</p>"
+
+
+# Exercise:
+# add a route /greeting that randomly selects a greeting from the following list  
+
+
 
 # run app on port 5000, debug=True reruns the app on code changes
 app.run(port=5000, debug=True)
