525f4bfd017224ebb8befe4ad3d0d24cc8ecf893
dom-edu <84454639+dom-edu@users.noreply.github.com>
2025-07-26 12:04:56 -0400

    added comments for app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+# importing the Flask object from the flask module
+# (that we installed with pip3 install flask)
 from flask import Flask
 
+# instantiating the flask app 
 app = Flask(__name__)
 
+# register home route (/) to app
 @app.route("/")
 def hello_world():
+    """ 
+    return hello world to user 
+    """
     return "<p>Hello, World!</p>"
 
 # run app on port 5000
