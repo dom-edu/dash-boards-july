13bb55b66f647b4c581b0dce167e2584a708832a
DomDomDoy <domilamad89@gmail.com>
2025-08-02 13:18:32 -0400

    added cities map

--- us-cities-app.py
+++ us-cities-app.py
@@ -1,10 +1,11 @@
 import pandas as pd 
 import plotly.express as px
+import plotly.graph_objects as go
 from dash import Dash, html, dcc ,  Input, Output,callback
 
-url = "https://raw.githubusercontent.com/plotly/datasets/refs/heads/master/2014_us_cities.csv"
-
-cities_df = pd.read_csv(url)
+# loading in the data 
+URL = "https://raw.githubusercontent.com/plotly/datasets/refs/heads/master/2014_us_cities.csv"
+cities_df = pd.read_csv(URL)
 
 
 # too much data, let's slim it down to top_10
@@ -16,15 +17,15 @@ x = top_10_cities['name'] # get city names
 y = top_10_cities['pop'] # get city populations
 bar_ = px.bar(x=x, y=y)
 
-# drop down 
+# drop down component 
 # make cities 
 dd_1 = dcc.Dropdown(
     x,
     x[:2], # by default select first two 
     id="cities-dd",
+    placeholder="Select a city",
     multi=True
 )
-
 
 # change axis labels, usually 
 # how we edit styles in dash 
@@ -32,6 +33,46 @@ bar_.update_layout(
     xaxis_title = "City",
     yaxis_title = "Pop"
 )
+
+# format tooltip text 
+cities_df['text'] = cities_df['name'] + '<br>Population ' + (cities_df['pop']/1e6).astype(str)+' million'
+
+limits = [(0,3),(3,11),(11,21),(21,50),(50,3000)]
+colors = ["royalblue","crimson","lightseagreen","orange","lightgrey"]
+cities = []
+scale = 5000
+
+
+# empty frame 
+geo_scatter_ = go.Figure()
+
+for i in range(len(limits)):
+    lim = limits[i]
+    df_sub = cities_df[lim[0]:lim[1]]
+    geo_scatter_.add_trace(go.Scattergeo(
+        locationmode = 'USA-states',
+        lon = df_sub['lon'],
+        lat = df_sub['lat'],
+        text = df_sub['text'],
+        marker = dict(
+            size = df_sub['pop']/scale,
+            color = colors[i],
+            line_color='rgb(40,40,40)',
+            line_width=0.5,
+            sizemode = 'area'
+        ),
+        name = '{0} - {1}'.format(lim[0],lim[1])))
+
+geo_scatter_.update_layout(
+        title_text = '2014 US city populations<br>(Click legend to toggle traces)',
+        showlegend = True,
+        geo = dict(
+            scope = 'usa',
+            landcolor = 'rgb(217, 217, 217)',
+        )
+    )
+
+
 
 
 # instantiate the dash app 
@@ -41,7 +82,8 @@ app = Dash(__name__)
 app.layout = [
     html.Div(children="US City Populations", style={'textAlign':'center'}),
     dd_1,
-    dcc.Graph(figure=bar_, id="cities-bar-chart")
+    dcc.Graph(figure=bar_, id="cities-bar-chart"),
+    dcc.Graph(figure=geo_scatter_, id="cities-geo-scatter")
 ]
 
 # we want to register a call back 
@@ -50,6 +92,16 @@ app.layout = [
     Input('cities-dd', 'value')
 )
 def update_graph(value):
+    """
+    
+    
+
+    Args:
+        value : selected cities 
+
+    Returns:
+        new bar chart with selected cities 
+    """
     print("DEBUG:", value)
 
     # filter the dataframe by selected value 
