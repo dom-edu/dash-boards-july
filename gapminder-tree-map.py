54b8a9fa726d31ccf4f64602333c566f5718ab7e
dom-edu <84454639+dom-edu@users.noreply.github.com>
2025-08-09 13:08:09 -0400

    added basic dash app shell

---
+++ gapminder-tree-map.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+import plotly.express as px
+import plotly.graph_objects as go
+from dash import Dash, html, dcc ,  Input, Output,callback
+
+
+# load in the gapminder data 
+URL = "https://raw.githubusercontent.com/plotly/datasets/refs/heads/master/gapminder_unfiltered.csv"
+
+# read into pandas dataframe  
+gapminder_df = pd.read_csv(URL)
+
+
+# let's instantiate the dash app 
+app = Dash(__name__) 
+
+# define layout to make dash happy 
+app.layout = []
+
+
+# let's run the app 
+app.run(debug=True,port=5001)
