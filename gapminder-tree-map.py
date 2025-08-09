34e5d6ca44394068ee879ddc511726a4fcc994e8
dom-edu <84454639+dom-edu@users.noreply.github.com>
2025-08-09 15:07:17 -0400

    removed extra empty lines

--- gapminder-tree-map.py
+++ gapminder-tree-map.py
@@ -38,9 +38,7 @@ def create_tree_map(df_):
                         #   hover_data=['iso_alpha'],
                         color_continuous_scale='RdBu',
                     color_continuous_midpoint=np.average(df_['lifeExp'], weights=gapminder_df['pop']))
-    
     return treemap_
-
 
 # create tree map
 treemap_ = create_tree_map(gapminder_df)
