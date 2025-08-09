b9c366ce4e3ee2ea836236292d66fd8b714ee265
dom-edu <84454639+dom-edu@users.noreply.github.com>
2025-08-09 14:00:04 -0400

    added treemap to layout

--- gapminder-tree-map.py
+++ gapminder-tree-map.py
@@ -1,3 +1,4 @@
+import numpy as np 
 import pandas as pd 
 import plotly.express as px
 import plotly.graph_objects as go
@@ -23,10 +24,24 @@ dd_1 = dcc.Dropdown(
     years_, 
     years_[0], 
     id='year-dd'
-)
+) 
+
+# tree map 
+treemap_ = px.treemap(gapminder_df, 
+                      path=[px.Constant("world"), 
+                            'continent', 
+                            'country'], 
+                      values='pop',
+                      color='lifeExp',
+                    #   hover_data=['iso_alpha'],
+                    color_continuous_scale='RdBu',
+                  color_continuous_midpoint=np.average(gapminder_df['lifeExp'], weights=gapminder_df['pop']))
 
 # define layout to make dash happy 
-app.layout = [dd_1]
+app.layout = [
+    dd_1,
+    dcc.Graph(figure=treemap_, id="treemap-fig")
+]
 
 
 # let's run the app 
