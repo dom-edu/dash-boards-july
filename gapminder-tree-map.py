2c21fc24e92b3805c955a51f52628a849b25bcdd
dom-edu <84454639+dom-edu@users.noreply.github.com>
2025-08-09 15:05:31 -0400

    added create_tree_map() for more modular code

--- gapminder-tree-map.py
+++ gapminder-tree-map.py
@@ -26,16 +26,24 @@ dd_1 = dcc.Dropdown(
     id='year-dd'
 ) 
 
-# tree map 
-treemap_ = px.treemap(gapminder_df, 
-                      path=[px.Constant("world"), 
-                            'continent', 
-                            'country'], 
-                      values='pop',
-                      color='lifeExp',
-                    #   hover_data=['iso_alpha'],
-                    color_continuous_scale='RdBu',
-                  color_continuous_midpoint=np.average(gapminder_df['lifeExp'], weights=gapminder_df['pop']))
+def create_tree_map(df_):
+
+    # tree map 
+    treemap_ = px.treemap(df_, 
+                        path=[px.Constant("world"), 
+                                'continent', 
+                                'country'], 
+                        values='pop',
+                        color='lifeExp',
+                        #   hover_data=['iso_alpha'],
+                        color_continuous_scale='RdBu',
+                    color_continuous_midpoint=np.average(df_['lifeExp'], weights=gapminder_df['pop']))
+    
+    return treemap_
+
+
+# create tree map
+treemap_ = create_tree_map(gapminder_df)
 
 # define layout to make dash happy 
 app.layout = [
@@ -56,21 +64,9 @@ def update_graph(value):
     filtered_df = gapminder_df[filter_]
 
     # make a new treemap 
-    new_tree_map = px.treemap(filtered_df, 
-        path=[px.Constant("world"), 
-            'continent', 
-            'country'], 
-        values='pop',
-        color='lifeExp',
-    #   hover_data=['iso_alpha'],
-    color_continuous_scale='RdBu',
-    color_continuous_midpoint=np.average(filtered_df['lifeExp'], weights=filtered_df['pop']))
+    new_tree_map = create_tree_map(filtered_df)
 
     # return the new tree map to be updated in the graph object 
     return new_tree_map
-
-
-
-
 # let's run the app 
 app.run(debug=True,port=5001)
